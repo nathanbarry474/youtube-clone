b1fc7ff9b5c5d9b3e2a26044fa46c72798fec01b
Nathan Barry <Nathanbarry474@gmail.com>
2020-07-16 22:39:52 -0500

    completed project (no API key)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Grid from "@material-ui/core/Grid";
 
@@ -9,17 +9,40 @@ import VideoList from "./components/VideoList";
 import youtube from "./api/youtube";
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const handleSubmit = async (searchTerm) => {
+    const response = await youtube.get("search", {
+      params: {
+        q: searchTerm,
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyDBn5EOy4DkXxbO6Sz4qq2COepedADLhBM",
+      },
+    });
+
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+  };
+
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
+  };
+
   return (
-    <Grid container justify="center" spacing={16}>
+    <Grid container justify="center" spacing={10}>
       <Grid item xs={12}>
-        <Grid container spacing={16}>
+        <Grid container spacing={10}>
           <Grid item xs={12}>
-            <SearchBar />
+            <SearchBar onFormSubmit={handleSubmit} />
           </Grid>
           <Grid item xs={8}>
-            <VideoDetail />
+            <VideoDetail video={selectedVideo} />
           </Grid>
-          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const VideoDetail = () => {
-  return <h1>Video List</h1>;
+import { Grid } from "@material-ui/core";
+
+import VideoItem from "./VideoItem";
+
+const VideoList = ({ videos, onVideoSelect }) => {
+  const listOfVideos = videos.map((video, id) => (
+    <VideoItem onVideoSelect={onVideoSelect} key={id} video={video} />
+  ));
+  return <Grid container spacing={10}>{listOfVideos}</Grid>;
 };
 
-export default VideoDetail;
+export default VideoList;
